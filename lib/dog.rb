e7371894bd6af2f1615c7c57801196484f944e0b
Bishnu <bishnuthapako@gmail.com>
2022-07-19 13:54:21 -0400

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
-class Dog
+require "pry"
 
-end
+class Dog
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+
+lassie = Dog.new
+carlos = Dog.new
+lassie.name = "Lassie"
+carlos.name = "Carlos"
+
+puts lassie.name
+puts carlos.name
